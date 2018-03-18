6da556bb13e92ee59e7ad3e1c70e87c7c0ecaffd
Aneesh Thattanthody <aneesht90@gmail.com>
2018-03-18 09:20:06 +0100

    updated SegNet-Basic - tensorboard added

--- SegNet-Basic.py
+++ SegNet-Basic.py
@@ -32,12 +32,13 @@ data_shape = 360*480
 
 class_weighting= [0.2595, 0.1826, 4.5640, 0.1417, 0.5051, 0.3826, 9.6446, 1.8418, 6.6823, 6.2478, 3.0, 7.3614]
 
-nb_epoch = 100
+nb_epoch = 1
 batch_size = 3
 
 
 
-
+tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
+                              write_graph=True, write_images=False)
 
 
 # load the model:
@@ -50,7 +51,8 @@ segnet_basic.compile(loss="categorical_crossentropy", optimizer='adadelta', metr
 # checkpoint
 filepath="weights.best.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-callbacks_list = [checkpoint]
+#callbacks_list = [checkpoint]
+callbacks_list = [tensorboard]
 
 
 
@@ -58,13 +60,20 @@ callbacks_list = [checkpoint]
 train_data = np.load('./data/train_data.npy')
 train_label = np.load('./data/train_label.npy')
 
-test_data = np.load('./data/test_data.npy')
-test_label = np.load('./data/test_label.npy')
+
+valid_data = np.load('./data/val_data.npy')
+valid_label = np.load('./data/val_label.npy')
 
 
 # Fit the model
-history = segnet_basic.fit(train_data, train_label, callbacks=callbacks_list, batch_size=batch_size, epochs=nb_epoch,
-                    verbose=1, class_weight=class_weighting , validation_data=(test_data, test_label), shuffle=True) # validation_split=0.33
+history = segnet_basic.fit(train_data, train_label, batch_size=batch_size, epochs=nb_epoch,
+                    verbose=1, class_weight=class_weighting , validation_data=(valid_data, valid_label),
+                    shuffle=True,callbacks=callbacks_list) # validation_split=0.33
+
+
+
+
+
 
 # This save the trained model weights to this file with number of epochs
 segnet_basic.save_weights('weights/model_weight_{}.hdf5'.format(nb_epoch))
