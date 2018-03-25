1ca57838f9128b7bc884d5bae370c0e3c913e132
Aneesh Thattanthody <aneesht90@gmail.com>
2018-03-25 17:40:06 +0200

    updated helper function with parameters

--- helper.py
+++ helper.py
@@ -8,6 +8,13 @@ import itertools
 from helper import *
 import os
 
+
+
+image_height  = 192
+image_width   = 256
+segment_count = 12
+
+
 def normalized(rgb):
     #return rgb/255.0
     norm=np.zeros((rgb.shape[0], rgb.shape[1], 3),np.float32)
@@ -23,8 +30,11 @@ def normalized(rgb):
     return norm
 
 def one_hot_it(labels):
-    x = np.zeros([360,480,12])
-    for i in range(360):
-        for j in range(480):
+    x = np.zeros([image_height,image_width,segment_count])
+    for i in range(image_height):
+        for j in range(image_width):
+            # if labels[i][j]>=12:
+            #     labels[i][j]=11
+                #print("index is 12")
             x[i,j,labels[i][j]]=1
     return x
